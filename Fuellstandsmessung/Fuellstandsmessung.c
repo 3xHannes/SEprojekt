e8fd04e3ae85b3657573a04129076182702d7f66
Tobias Roesch <toroe.facebook@web.de>
2015-03-02 20:52:58 +0100

    Initial Commit des leeren Projektes

---
+++ Fuellstandsmessung/Fuellstandsmessung.c
@@ -0,0 +1,17 @@
+/*
+ * Fuellstandsmessung.c
+ *
+ * Created: 02.03.2015 20:43:17
+ *  Author: Tobias
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
